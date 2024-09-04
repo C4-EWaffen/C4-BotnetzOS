b279efa69773dd661efa8a5678d90f6337585a9c
C4-E-Waffen Repositorys <56237692+C4-EWaffen@users.noreply.github.com>
2024-09-04 12:38:52 +0000

    Create config.php

---
+++ Krypto/config.php
@@ -0,0 +1,6 @@
+<?php
+$db_host = 'localhost';  // Ersetzen Sie dies durch Ihren Datenbank-Host
+$db_name = 'database_name';  // Ersetzen Sie dies durch Ihren Datenbanknamen
+$db_user = 'username';  // Ersetzen Sie dies durch Ihren Datenbank-Benutzernamen
+$db_pass = 'password';  // Ersetzen Sie dies durch Ihr Datenbank-Passwort
+?>
